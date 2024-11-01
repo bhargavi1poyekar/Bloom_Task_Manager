05863734a32bd970a720fbb731c61a2bae218e73
bhargavi <bhargavi1poyekar@gmail.com>
2024-10-31 23:10:58 -0400

    Updated Readme

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -12,7 +12,7 @@ class TaskController extends Controller
      */
     public function index()
     {
-        $tasks = Task::paginate(2);
+        $tasks = Task::paginate(10);
         return view('tasks.index', compact('tasks'));
 
     }
